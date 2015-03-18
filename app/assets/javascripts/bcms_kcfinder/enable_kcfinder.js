895ab82479c77b32d1289991439dcbbd50af3a1e
Paweł Łuczak <woochaq@gmail.com>
2015-03-18 09:45:48 +0100

    Italic style for ckeditor

--- app/assets/javascripts/bcms_kcfinder/enable_kcfinder.js
+++ app/assets/javascripts/bcms_kcfinder/enable_kcfinder.js
@@ -1,8 +1,14 @@
 // Additional configuration to enable KCFinder
 
 //CKEDITOR.editorConfig = function(config) {
-   CKEDITOR.config.filebrowserBrowseUrl = '/bcms_kcfinder/browse';
-   CKEDITOR.config.filebrowserImageBrowseUrl = '/bcms_kcfinder/browse?type=images';
+    CKEDITOR.config.filebrowserBrowseUrl = '/bcms_kcfinder/browse';
+    CKEDITOR.config.filebrowserImageBrowseUrl = '/bcms_kcfinder/browse?type=images';
+
+    CKEDITOR.config.coreStyles_italic =
+    {
+      element : 'em',
+      attributes : { 'style' : 'font-style: italic' }
+    };
 //   config.filebrowserFlashBrowseUrl = '/assets/kcfinder/browse.php?type=flash';
 //   config.filebrowserUploadUrl = '/assets/kcfinder/upload.php?type=files';
 //   config.filebrowserImageUploadUrl = '/assets/kcfinder/upload.php?type=images';
