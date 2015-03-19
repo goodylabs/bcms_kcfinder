1c4b1211021c685a1bd5562d62387f263a26dbd0
Paweł Łuczak <woochaq@gmail.com>
2015-03-19 08:21:42 +0100

    Remove inline style from em tag

--- app/assets/javascripts/bcms_kcfinder/enable_kcfinder.js
+++ app/assets/javascripts/bcms_kcfinder/enable_kcfinder.js
@@ -4,11 +4,6 @@
     CKEDITOR.config.filebrowserBrowseUrl = '/bcms_kcfinder/browse';
     CKEDITOR.config.filebrowserImageBrowseUrl = '/bcms_kcfinder/browse?type=images';
 
-    CKEDITOR.config.coreStyles_italic =
-    {
-      element : 'em',
-      attributes : { 'style' : 'font-style: italic' }
-    };
 //   config.filebrowserFlashBrowseUrl = '/assets/kcfinder/browse.php?type=flash';
 //   config.filebrowserUploadUrl = '/assets/kcfinder/upload.php?type=files';
 //   config.filebrowserImageUploadUrl = '/assets/kcfinder/upload.php?type=images';
